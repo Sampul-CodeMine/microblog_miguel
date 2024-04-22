2586a2acc4d95c1999ca72e1e9223f9507efd09c
Sampul-CodeMine <studysampul@gmail.com>
2024-04-22 17:29:05 +0100

    Added Functionalities to handle timezones for post

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
 import os
@@ -13,6 +14,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 mail = Mail(app)
 login.login_view = 'signin'
